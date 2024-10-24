c9ee1c4d2eed97803556ece59ec8014280917549
David Owen <owend91@gmail.com>
2024-10-24 13:23:21 -0600

    Initial Commit

---
+++ FetchRecipeProject/FetchRecipeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeProjectApp.swift
+//  FetchRecipeProject
+//
+//  Created by David Owen on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
